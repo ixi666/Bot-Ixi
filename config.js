b3d76a4a2abd82071568b923ba453e3b093f92f4
ixi666 <103534276+ixi666@users.noreply.github.com>
2022-06-03 13:18:11 -0600

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ global.owner = [
   ['526565978886', ' Ixxi - Creador ', true],
   ['79774424276', ' Nymos  ', true],
   ['522312000993', ' Penudo63 ', true]
-  ['50555029450', ' Melon perra de ixi ', true]
+  ['50555029450', ' Melon  ', true]
 ] // Cambia los numero que quieras
 global.mods = [] 
 global.prems = [] 
